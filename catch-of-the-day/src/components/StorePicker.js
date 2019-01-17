bf196cd6ccfe31717068e132ad49b5d085d667a3
Timo Kirkkala <timo.kirkkala@wunder.io>
2019-01-17 11:49:24 +0200

    Step 3: Add StorePicker initial component with comments

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+// Always import react to the components!
+import React from "react";
+
+// Stir the sauce.
+class StorePicker extends React.Component {
+  render() {
+    return <p>Here is a store picker</p>;
+  }
+}
+
+// Always export to be available.
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+
+// Import my custom components.
+import StorePicker from "./components/StorePicker";
+
+// render(<p>Hohoo!</p>, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
