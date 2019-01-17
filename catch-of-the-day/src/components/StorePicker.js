4982aa298583d2be94e33e024f5845a05ac38e33
Timo Kirkkala <timo.kirkkala@wunder.io>
2019-01-17 14:31:16 +0200

    Step 11: Events, refs and binding
    Notice the syntaxt to not need (commented) constructor:
    `methodName = event => { ...`

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,15 +4,35 @@ import { getFunName} from "../helpers";
 
 // Stir the sauce.
 class StorePicker extends React.Component {
+
+  // constructor() {
+  //   super()
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  // Notice uncommented syntax to not need the constructor above
+  // BLAH: goToStore(event) { ...
+  goToStore = event => {
+    // 1. Stop form from submitting
+    event.preventDefault();
+    // 2. Get text from input
+    console.log(this);
+
+    // 3. Change the page to /store/input
+  }
+
   render() {
     return (
       <React.Fragment>
         {/* For comments, use block comments within curly brackets */}
         {/* <React.Fragment> should also work as empty tag (<>) */}
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
           <h2>Enter a store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store name"
             defaultValue={getFunName()}
