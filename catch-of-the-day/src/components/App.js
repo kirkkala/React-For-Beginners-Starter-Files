bde4e635b4f4ef1d5a0178a3c226b8cf63c2de35
Timo Kirkkala <timo.kirkkala@wunder.io>
2019-01-17 16:22:03 +0200

    Step 14: Load sample set of fishes (loading data into state onClick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -20,6 +21,9 @@ class App extends React.Component {
       fishes // equals to "fishes: fishes" because same name
     });
   };
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes}); 
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -27,7 +31,10 @@ class App extends React.Component {
           <Header tagline="Fresh seafood market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
