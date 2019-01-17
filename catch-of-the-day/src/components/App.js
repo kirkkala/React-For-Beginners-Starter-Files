2306026b66a5c6a8d3bc97778e5c55795a7479f0
Timo Kirkkala <timo.kirkkala@wunder.io>
2019-01-17 16:55:24 +0200

    Step 15: Displaying state with JSX
    Load sample set to catch of the day menu

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -22,13 +23,18 @@ class App extends React.Component {
     });
   };
   loadSampleFishes = () => {
-    this.setState({fishes: sampleFishes}); 
+    this.setState({fishes: sampleFishes});
   };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh seafood market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
